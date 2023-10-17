1be087c709b5e9583bf2770b7b4bd987196fa355
tomasvivaldi <antonio.tomas.vivaldi@gmail.com>
2023-10-17 19:46:13 +0200

    image adjustments

--- components/ImageSlider.tsx
+++ components/ImageSlider.tsx
@@ -43,7 +43,7 @@ const ImageSlider = () => {
             layout="fill"
             objectFit="cover"
             objectPosition={
-              index === 0 ? "top right" : index === 1 ? " center" : "top"
+              index === 0 ? "top right" : index === 1 ? "top center" : "top"
             }
           />
         </div>
