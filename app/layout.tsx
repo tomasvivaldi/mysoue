a2b46936f5b68e2219fe96cfa764cc9e561f8fd9
tomasvivaldi <antonio.tomas.vivaldi@gmail.com>
2024-01-03 14:02:39 +0100

    added website info

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import "./globals.css";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "World Scan Project - Jikai",
-  description: "World Scan Project's Jikai Website",
+  title: "My Soue",
+  description: "Make your sharable wishlist today!",
 };
 
 export default function RootLayout({
