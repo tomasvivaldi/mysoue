8c1b585e76c6cd91cfd0016a44992e69e9f744d5
tomasvivaldi <antonio.tomas.vivaldi@gmail.com>
2025-04-21 14:35:20 +0900

    Enhance WishlistSelectionModal with improved styling for better usability. Added max-height and overflow properties to ensure content is scrollable and fits within the modal view.

--- components/aline_design/modals/WishlistSelectionModal.tsx
+++ components/aline_design/modals/WishlistSelectionModal.tsx
@@ -146,13 +146,13 @@ const WishlistSelectionModal: React.FC<WishlistSelectionModalProps> = ({ isOpen,
   };
 
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
-      <div className="relative w-[90%] sm:w-[70%] lg:w-[40%] bg-white rounded-lg shadow-lg p-8">
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 max-h-screen p-10">
+      <div className="relative w-[90%] sm:w-[70%] lg:w-[40%] bg-white rounded-lg shadow-lg p-8 max-h-full overflow-y-auto">
         <h2 className="text-2xl font-bold mb-4">{t("select_a_wishlist")}</h2>
         {wishlists.length === 0 ? (
           <p>{t("no_wishlists_found")}</p>
         ) : (
-          <ul className="flex flex-col gap-2">
+          <ul className="flex flex-col gap-2 ">
             {wishlists.map((wishlist: any) => {
                 const alreadyAdded = wishlist.wishlist_items && wishlist.wishlist_items.some((item: any) => item.product_id === productId);
                 return (
