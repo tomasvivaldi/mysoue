fc2aaddb009414ac1b7d913fb6feabf2f95271d4
tomasvivaldi <antonio.tomas.vivaldi@gmail.com>
2025-05-20 18:10:21 +0900

    Add translation support for ConfirmationStep

--- components/ConfirmationStep.tsx
+++ components/ConfirmationStep.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import { Button2 } from "./buttons/Button2";
 import GhostButtonBlack from "./GhostButtonBlack";
 import SolidButtonBlack from "./SolidButtonBlack";
+import { useTranslations } from "next-intl";
 // import { useRouter } from "next/router";
 
 const ConfirmationStep: React.FC = () => {
+  const t = useTranslations("ConfirmationStep");
   //   const router = useRouter();
 
   //   const handleCheckWishlists = () => {
@@ -17,17 +19,17 @@ const ConfirmationStep: React.FC = () => {
 
   return (
     <div className="flex flex-col items-center justify-center p-4 h-full w-full">
-      <div className="rounded-md border-gray-200 bg-[#fbf9f4] px-4 md:px-12 py-8 w-[90%] sm:w-[80%] lg:w-[50%] mx-auto shadow-xl text-center">
-        <h2 className="text-2xl font-semibold mb-4">List Added Successfully</h2>
+      <div className="rounded-md border-gray-200 bg-[#fbf9f4] px-4 md:px-12 py-8 w-[90%] sm:w-[80%]  mx-auto shadow-xl text-center">
+        <h2 className="text-2xl font-semibold mb-4">{t("title")}</h2>
         <p className="text-gray-600 mb-8">
-          This list was added successfully to MySoue Wishlists
+          {t("description")}
         </p>
         <div className="flex flex-col lg:flex-row gap-4 justify-center items-center">
           <a href="/dashboard/mysoue-lists">
-            <GhostButtonBlack text="Check MySoue Wishlists" />
+            <GhostButtonBlack text={t("checkWishlists")} />
           </a>
           <a href="/dashboard/my-wishlists">
-            <SolidButtonBlack text="Back to your lists" />
+            <SolidButtonBlack text={t("backToLists")} />
           </a>
         </div>
       </div>
