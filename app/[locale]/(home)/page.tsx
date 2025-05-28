13526a19f39189d4c7d9b38461773a8ba83d609a
tomasvivaldi <antonio.tomas.vivaldi@gmail.com>
2025-05-28 18:15:14 +0900

    deleted not used code

--- app/[locale]/(home)/page.tsx
+++ app/[locale]/(home)/page.tsx
@@ -12,7 +12,6 @@ import VideoBanner from "@/components/aline_design/VideoBanner";
 import VisionBoard from "@/components/aline_design/VisionBoard";
 import Wishes from "@/components/aline_design/Wishes";
 import WishlistsFeatures from "@/components/aline_design/WishlistsFeatures";
-
 import { RedLogosBanner } from "@/components/banners/RedLogosBanner";
 import { SmallTextBanner } from "@/components/banners/SmallTextBanner";
 import SolidButton from "@/components/buttons/SolidButton";
@@ -23,39 +22,6 @@ import { getLocale } from "next-intl/server";
 const HomePage = () => {
   const locale = getLocale();
   const t = useTranslations("SmallTextBanner");
-  const testimonials = [
-    {
-      text: "Orci Vel Eget In Eu. Integer Amet Porttitor Hendrerit Etiam Arcu, Aliquet Duis Pretium Consequat. Semper Sed Viverra Enim Ut Nunc.",
-      author: "Courtney Henry",
-      avatarUrl: "/avatars/courtney-henry.png",
-      rating: 5,
-    },
-    {
-      text: "Tincidunt Risus, Blandit Proin Semper. Tellus Ac Pellentesque Convallis Vitae. Lorem Enim Cursus Et Consequat Viverra Id Justo Ullamcorper. Vel.",
-      author: "Darrell Steward",
-      avatarUrl: "/avatars/darrell-steward.png",
-      rating: 4,
-    },
-    {
-      text: "Fusce Egestas Tellus Rutrum Tellus Pellentesque Eu Tincidunt Tortor Aliquam. Nulla Pellentesque Dignissim Enim Sit Amet Venenatis.",
-      author: "Theresa Webb",
-      avatarUrl: "/avatars/theresa-webb.png",
-      rating: 5,
-    },
-    {
-      text: "Maecenas Sed Enim Ut Sem Viverra Aliquet. Nec Sagittis Aliquam Malesuada Bibendum Arcu Vitae Elementum Curabitur.",
-      author: "Cody Fisher",
-      avatarUrl: "/avatars/cody-fisher.png",
-      rating: 4,
-    },
-    {
-      text: "Sed Risus Pretium Quam Vulputate Dignissim Suspendisse In Est Ante In Nibh Mauris Cursus Mattis.",
-      author: "Esther Howard",
-      avatarUrl: "/avatars/esther-howard.png",
-      rating: 5,
-    },
-  ];
-
   return (
     <>
       <SolidButton text="Start Now" href="/login" className="text-xl px-12 py-4 mx-10 my-4 bg-[#FFF9E8] text-black hover:text-white hover:bg-[#A5282C]" />
